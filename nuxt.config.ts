d3ddff5903d31f0978bfbf07796178b80b423769
Evgeniy-Shabakov <shabakovevgeniy@gmail.com>
2025-04-30 09:29:04 +0300

    Browser icon - Adde link on server

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,23 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 
 import tailwindcss from "@tailwindcss/vite";
+import { serverUrl } from "./env"
 
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: false },
-  css: ['~/assets/css/main.css'],
-  vite: {
-   plugins: [
-     tailwindcss(),
-   ],
- },
+   compatibilityDate: '2024-11-01',
+   devtools: { enabled: false },
+   css: ['~/assets/css/main.css'],
+   app: {
+      head: {
+         // title: 'Админка',
+         link: [
+            { rel: 'icon', type: 'image/x-icon', href: `${serverUrl}/storage/images/favicon.png` },
+         ]
+      }
+   },
+   vite: {
+      plugins: [
+         tailwindcss(),
+      ],
+   },
 })
