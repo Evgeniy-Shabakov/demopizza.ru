ddc40b58efec3269488058aa813480d3e342b62c
Evgeniy-Shabakov <shabakovevgeniy@gmail.com>
2025-05-02 06:26:59 +0300

    Index Page - Added nav

--- pages/index.vue
+++ pages/index.vue
@@ -33,58 +33,78 @@ function scrollToCategory(index) {
       behavior: "smooth",
    })
 }
-
-
-
 </script>
 
 <template>
 
-   <div>
+   <header class="bg-(--background-page-elements-color)">
 
-      <header class="bg-(--background-page-elements-color)">
+      <section class="shadow-md hidden md:block">
 
-         <section class="shadow-md hidden md:block">
+         <div class="my-container py-3">
+            <BaseLink to='/legal-documents'>Правовая информация</BaseLink>
+         </div>
 
-            <div class="my-container py-3">
-               <BaseLink to='/legal-documents'>Правовая информация</BaseLink>
+      </section>
+
+      <section class="my-container">
+
+         <div class="flex items-center justify-between py-1 md:py-2 lg:py-4 ">
+
+            <div class="flex items-center gap-3.5">
+               <img v-if="company"
+                    class=" h-10 w-10 lg:h-20 lg:w-20"
+                    :src="company.logo_url">
+               <div v-if="company">
+                  <h1 class="text-2xl font-bold lg:text-4xl">{{ company.brand_title }}</h1>
+                  <div class="hidden lg:block">{{ company.tagline }}</div>
+               </div>
             </div>
 
-         </section>
-
-         <section class="my-container">
-
-            <div class="flex items-center justify-between py-1 md:py-2 lg:py-4 ">
-
-               <div class="flex items-center gap-3.5">
-                  <img v-if="company"
-                       class=" h-10 w-10 lg:h-20 lg:w-20"
-                       :src="company.logo_url">
-                  <div v-if="company">
-                     <h1 class="text-2xl font-bold lg:text-4xl">{{ company.brand_title }}</h1>
-                     <div class="hidden lg:block">{{ company.tagline }}</div>
-                  </div>
-               </div>
-
-               <div class="hidden md:block">
-                  <BaseLink v-if="authUser"
-                            to="/user">Личный кабинет</BaseLink>
-                  <BaseLink v-else
-                            to="/login">Войти</BaseLink>
-               </div>
-
-               <BurgerMenu class="md:hidden" />
-
+            <div class="hidden md:block">
+               <BaseLink v-if="authUser"
+                         to="/user">Личный кабинет</BaseLink>
+               <BaseLink v-else
+                         to="/login">Войти</BaseLink>
             </div>
 
-         </section>
+            <BurgerMenu class="md:hidden" />
 
-      </header>
+         </div>
 
+      </section>
+
+   </header>
+
+   <nav ref="categoriesMenu"
+        class="bg-(--background-page-elements-color) shadow-md py-2 overflow-x-auto lg:pb-3">
       <div class="my-container">
-         index
+
+         <div v-if="categories"
+              ref="categoriesMenuInner"
+              class="flex gap-1 sm:gap-2 lg:flex-wrap">
+
+            <div v-for="(category, index) in categories"
+                 class="px-3 py-0.5 shadow-md text-nowrap
+                        bg-(--background-page-main-color) rounded-(--border-radius-categories-menu)
+                        hover:text-(--brand-color) active:text-(--brand-color)">
+               <a ref="categoriesItems"
+                  :key="index"
+                  @click="scrollToCategory(index)"
+                  :href="'#' + category.title">
+                  {{ category.title }}
+               </a>
+            </div>
+
+         </div>
+
       </div>
-      
+   </nav>
+
+   <div class="my-container">
+      index
    </div>
+
+
 
 </template>
