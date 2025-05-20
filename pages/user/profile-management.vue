cf1dafb4d45269cf94a7b04b6f27221907ae879e
Evgeniy-Shabakov <shabakovevgeniy@gmail.com>
2025-05-20 09:06:39 +0300

    Profile management page - Added all elements

--- pages/user/profile-management.vue
+++ pages/user/profile-management.vue
@@ -1,11 +1,47 @@
- <script setup>
- </script>
- 
- <template>
-    <div>
-      profile-management
-    </div>
- </template>
- 
- <style scoped>
- </style>
+<script setup>
+import { logoutClient } from '~/js/client-helper.js'
+import { authUser } from '~/js/axios-helper.js'
+import { formatPhone } from '~/js/phone-helper'
+
+function logoutVue() {
+   logoutClient()
+   navigateTo('/')
+}
+</script>
+
+<template>
+
+   <PopupPageWrapper>
+
+      <h1 class="text-xl text-center">
+         <div class="font-semibold">Управление профилем</div>
+         <div v-if="authUser"
+              class="text-base">
+            {{ formatPhone(authUser.phone) }}
+         </div>
+      </h1>
+
+      <div v-if="authUser">
+
+         <BaseButton class="w-full"
+                     :isIcon="true"
+                     :click="logoutVue">
+            Выйти из профиля
+         </BaseButton>
+
+      </div>
+
+      <BaseSpinner v-else />
+
+      <template #actions>
+
+         <BaseButton class="base-btn-in-popup-page"
+                     :click="() => navigateTo('/user')">
+            Назад
+         </BaseButton>
+
+      </template>
+
+   </PopupPageWrapper>
+
+</template>
