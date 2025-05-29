433a20f41e21d6a0cb749a928dd0ff4bdbd600d4
Evgeniy-Shabakov <shabakovevgeniy@gmail.com>
2025-05-29 13:44:33 +0300

    Legal documents page - Added links for download

--- pages/legal-documents.vue
+++ pages/legal-documents.vue
@@ -1,10 +1,32 @@
 <script setup>
+import { getModelsAxios, legalDocuments } from '~/js/axios-helper.js'
+
+if(!legalDocuments.value) getModelsAxios('legal-documents')
 </script>
 
 <template>
-   <PopupPageWrapper>
-      legal-documents
+   <PopupPageWrapper class="text-center">
+
+      <h1 class="text-xl text-center font-semibold">Правовая информация</h1>
+
+      <template v-if="legalDocuments">
+         <BaseLink v-for="document in legalDocuments"
+                   :href="document.file_url">
+            {{ document.title }}
+            (скачать)
+         </BaseLink>
+      </template>
+
+      <BaseSpinner v-else/>
+
+      <template #actions>
+
+         <BaseButton class="w-full"
+                     :click="() => navigateTo('/')">
+            Назад
+         </BaseButton>
+
+      </template>
+
    </PopupPageWrapper>
 </template>
-
-<style scoped></style>
