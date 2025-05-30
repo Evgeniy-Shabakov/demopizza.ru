6cadee49ce3ee772d74dba870e13dac2227ecc43
Evgeniy-Shabakov <shabakovevgeniy@gmail.com>
2025-05-30 13:42:28 +0300

    Auth.global.js - Refactoring

--- middleware/auth.global.js
+++ middleware/auth.global.js
@@ -1,11 +1,11 @@
 import { authUser } from '~/js/axios-helper.js'
 import { loginForOrder } from '~/js/login-panel-helper.js'
 
-const routeNamesForAuthUser = ['user', 'order-panel']
+const protectedPaths = ['user', 'order-panel', 'order-status'];
 
 export default defineNuxtRouteMiddleware(async (to, from) => {
    try {
-      if (!authUser.value && routeNamesForAuthUser.includes(to.name)) {
+      if (!authUser.value && protectedPaths.some(path => to.path.includes(path))) {
 
          if (!authUser.value) {
             if (to.name == 'order-panel') loginForOrder.value = true
