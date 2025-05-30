5385067a51a980aa6aadf97933edd3d47c47e746
Evgeniy-Shabakov <shabakovevgeniy@gmail.com>
2025-05-30 13:17:21 +0300

    Error page - Added

---
+++ error.vue
@@ -0,0 +1,29 @@
+<script setup>
+defineProps(['error'])
+</script>
+
+<template>
+   <div class="error">
+      <div class="text-5xl">
+         {{ error.statusCode }}
+      </div>
+      <div>
+         {{ error.message }}
+      </div>
+      <BaseButton :click="() => navigateTo('/')">
+         На главную
+      </BaseButton>
+   </div>
+</template>
+
+<style scoped>
+.error {
+   height: 100vh;
+   display: flex;
+   flex-direction: column;
+   align-items: center;
+   justify-content: center;
+   gap: 15px;
+   text-align: center;
+}
+</style>
