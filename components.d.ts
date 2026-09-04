399b89ebc1b5ef04c5f3580ffb390c61bd1c70a1
Evgeniy-Shabakov <shabakovevgeniy@gmail.com>
2026-09-04 16:04:04 +0300

    login

--- components.d.ts
+++ components.d.ts
@@ -85,6 +85,7 @@ declare module 'vue' {
     Textarea: typeof import('./src/shadcn/ui/textarea/Textarea.vue')['default']
     ThemeToggle: typeof import('./src/components/theme/ThemeToggle.vue')['default']
     TotalBlock: typeof import('./src/components/shared/TotalBlock.vue')['default']
+    Vkid: typeof import('./src/components/login-page/Vkid.vue')['default']
     WorkSchedule: typeof import('./src/components/shared/WorkSchedule.vue')['default']
   }
 }
--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -1,4 +1,91 @@
+<script setup>
+import { authUser } from '@/composables/useAuthUser'
+import { api } from '@/api/api'
+
+const router = useRouter()
+
+const errorVKAuth = ref()
+const errorLoginOnServer = ref()
+const isCheckedSoglasie = ref(false)
+
+function clearErrors() {
+   errorVKAuth.value = null
+   errorLoginOnServer.value = null
+}
+
+function handleErrorVKAuth(err) {
+   clearErrors()
+   errorVKAuth.value = err
+}
+
+async function handleSuccessVKAuth(access_token) {
+   clearErrors()
+
+   try {
+      const res = await api.post('/auth/login',
+         {
+            vkidAccessToken: access_token
+         })
+
+      authUser.value = res.data.data
+      console.log(authUser.value)
+
+      router.push('/profile')
+   }
+   catch (error) {
+      errorLoginOnServer.value = error
+   }
+}
+
+const authRef = ref(null)
+
+function simulateVkClick() {
+   if (!isCheckedSoglasie.value) return
+   authRef.value?.loginVK()
+}
+
+</script>
+
 <template>
-   <PageHeader>Login</PageHeader>
-   <BottomBar />
+   <div class="flex-1 flex flex-col justify-between items-center">
+
+      <PageHeader>
+         Вход в личный кабинет
+      </PageHeader>
+
+      <div>
+         <div :class="{ 'pointer-events-none cursor-not-allowed': !isCheckedSoglasie }">
+            <Vkid ref="authRef"
+                  @success="handleSuccessVKAuth"
+                  @error="handleErrorVKAuth" />
+         </div>
+
+         <div v-if="errorVKAuth"
+              class="text-center">
+            <div class="text-destructive">Ошибка ВК (авторизация)</div>
+            <div>Код: {{ errorVKAuth.code }}</div>
+            <div>Ошибка: {{ errorVKAuth.error }}</div>
+         </div>
+
+         <div v-if="errorLoginOnServer"
+              class="text-center">
+            <div class="text-destructive">
+               Ошибка входа на сервере
+            </div>
+            <div>{{ errorLoginOnServer }}</div>
+            <div>{{ errorLoginOnServer?.response?.data?.message }}</div>
+         </div>
+      </div>
+
+      <Soglasie v-model="isCheckedSoglasie"
+                class="mx-2" />
+
+   </div>
+
+   <BottomBar>
+      <ButtonLgWfull :disabled="!isCheckedSoglasie"
+                     @click="simulateVkClick">
+         Продолжить с VK ID
+      </ButtonLgWfull>
+   </BottomBar>
 </template>
