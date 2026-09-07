be02825612e23ded91b983fc0ccc4391dcb9eb3f
Evgeniy-Shabakov <shabakovevgeniy@gmail.com>
2026-09-07 11:51:22 +0300

    logout

--- src/router/router.guard.controller.js
+++ src/router/router.guard.controller.js
@@ -1,6 +1,11 @@
 import { authUser, loadAuthUser, userIsLoaded } from '@/composables/useAuthUser'
 
-const PROTECTED_ROUTES = ['profile', 'profile-delivery-addresses', 'profile-edit', 'profile-order-history']
+const PROTECTED_ROUTES = [
+   'profile',
+   'profile-delivery-addresses',
+   'profile-edit',
+   'profile-order-history'
+]
 
 export async function routerGuardController(to) {
    if (PROTECTED_ROUTES.includes(to.name)) {
