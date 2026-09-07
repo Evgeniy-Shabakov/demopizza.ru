7c3e05a3c1e8dbab139c8c0adac4233f12b89f2c
Evgeniy-Shabakov <shabakovevgeniy@gmail.com>
2026-09-07 11:59:28 +0300

    logout

--- src/pages/profile/ProfileEditPage.vue
+++ src/pages/profile/ProfileEditPage.vue
@@ -13,6 +13,7 @@ async function handleLogout() {
 
 <template>
    <PageHeader>Управление профилем</PageHeader>
+   <div class="-mt-2 text-center">{{ formatPhone(authUser.phone) }}</div>
 
    <Button variant="destructive"
            :disabled="isLoggingOut"
