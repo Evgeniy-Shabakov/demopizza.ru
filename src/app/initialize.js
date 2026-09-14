a1499d0d2a3c4fec003fc4541f25668a57aaa8f6
Evgeniy-Shabakov <shabakovevgeniy@gmail.com>
2026-09-14 16:38:50 +0300

    pwa theme

--- src/app/initialize.js
+++ src/app/initialize.js
@@ -4,7 +4,6 @@ import { loadCompany } from '@/composables/use-company'
 import { loadCategories } from '@/composables/use-categories'
 import { loadAuthUser } from '@/composables/use-auth-user'
 import { pwaBtnInitialize } from '@/helpers/pwa/pwa-btn-initialize'
-import '@/composables/use-theme-color'
 
 import '@/api/api'
 import '@/api/api.interceptor'
