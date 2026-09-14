f0b39694052812315e76395a4f8bf29b02df68a2
Evgeniy-Shabakov <shabakovevgeniy@gmail.com>
2026-09-14 08:23:35 +0300

    product page

--- src/pages/product.page.vue
+++ src/pages/product.page.vue
@@ -1,8 +1,90 @@
 <script setup>
+import { Minus, Plus } from '@lucide/vue'
+import { categories, isLoadingCategories, loadCategories } from '@/composables/use-categories'
+import { itemsInCart, addProductToCart, minusProductFromCart } from '@/composables/use-cart'
+import { isProductInStopList } from '@/helpers/is-product-in-stop-list'
+
+const route = useRoute()
+const id = Number(route.params.id)
+
+if (!categories.value) loadCategories()
+
+const currentProduct = computed(() => {
+   if (!categories.value) return null
+   return categories.value
+      .flatMap(category => category.products)
+      .find(product => product.id == id)
+})
+
+const isInStopList = computed(() => isProductInStopList(currentProduct.value))
+
+const description = computed(() =>
+   currentProduct.value?.descriptionFull || currentProduct.value?.descriptionShort
+)
+
+const cartItem = computed(() =>
+   itemsInCart.value.find(item => item.productId == id)
+)
+
+const isNotFound = computed(() =>
+   !isLoadingCategories.value && categories.value && !currentProduct.value
+)
 </script>
 
 <template>
-   <PageHeader>Карточка товара</PageHeader>
+   <PageHeader v-if="currentProduct">{{ currentProduct.name }}</PageHeader>
+   <PageHeader v-else>Карточка товара</PageHeader>
 
-   <BottomBar />
+   <SpinnerCenter v-if="isLoadingCategories" />
+
+   <div v-else-if="isNotFound"
+        class="text-center mt-32">
+      Товар не найден
+   </div>
+
+   <template v-else-if="currentProduct">
+      <img :src="currentProduct.imagePath"
+           :alt="currentProduct.name"
+           class="w-60 mx-auto aspect-square rounded-2xl mb-4" />
+
+      <div class="text-2xl font-bold mb-2">
+         {{ currentProduct.priceDefault }} ₽
+      </div>
+
+      <p v-if="description"
+         class="text-muted-foreground text-sm">
+         {{ description }}
+      </p>
+   </template>
+
+   <BottomBar>
+      <template v-if="!currentProduct">
+         <ButtonLgWfull @click="$router.back()">
+            Назад
+         </ButtonLgWfull>
+      </template>
+
+      <ButtonLgWfull v-else-if="isInStopList"
+                     disabled>
+         Будет позже
+      </ButtonLgWfull>
+
+      <template v-else-if="cartItem && cartItem.quantity > 0">
+         <div class="flex items-center gap-6 justify-between w-full">
+            <ButtonIcon variant="outline"
+                        @click="minusProductFromCart(id)">
+               <Minus />
+            </ButtonIcon>
+            <span class="w-4 text-center font-medium tabular-nums">{{ cartItem.quantity }}</span>
+            <ButtonIcon @click="addProductToCart(id)">
+               <Plus />
+            </ButtonIcon>
+         </div>
+      </template>
+
+      <ButtonLgWfull v-else
+                     @click="addProductToCart(id)">
+         В корзину
+      </ButtonLgWfull>
+   </BottomBar>
 </template>
