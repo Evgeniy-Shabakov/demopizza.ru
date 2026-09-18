c51cfd441da580af0606542134a84ea124e4009d
Evgeniy-Shabakov <shabakovevgeniy@gmail.com>
2026-09-18 19:29:34 +0300

    pwa

--- vite.config.js
+++ vite.config.js
@@ -35,7 +35,7 @@ export default defineConfig(({ mode }) => {
                short_name: env.VITE_BRAND_NAME,
                start_url: '/',
                theme_color: '#ffffff',
-               background_color: '#121212',
+               background_color: '#F5F5F5',
                display: 'standalone',
                icons: [
                   { src: '/pwa-icon-192x192.png', sizes: '192x192', type: 'image/png' },
