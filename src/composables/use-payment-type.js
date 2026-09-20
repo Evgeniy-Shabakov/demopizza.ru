c794f3fb3dd1f13643cba0022efd5f5af24c5b54
Evgeniy-Shabakov <shabakovevgeniy@gmail.com>
2026-09-20 18:12:58 +0300

    payment type

--- src/composables/use-payment-type.js
+++ src/composables/use-payment-type.js
@@ -1,9 +1,21 @@
 import { PAYMENT_TYPE } from "@/constants/payment-type"
+import { company } from "@/composables/use-company"
 
-export const paymentTypeId = useLocalStorage(
-   'payment_type_id', Number(import.meta.env.VITE_PAYMENT_TYPE_BY_DEFAULT))
+export const paymentTypeId = useLocalStorage('payment_type_id', 0)
 
-export const AVAILABLE_PAYMENT_TYPES = Object.freeze(
-   Object.values(PAYMENT_TYPE).filter(paymentType =>
-      import.meta.env.VITE_ENABLED_PAYMENT_TYPE_IDS.includes(paymentType.ID))
-)
+export const AVAILABLE_PAYMENT_TYPES = computed(() => {
+   const enabledIds = company.value?.options?.paymentTypeSetting?.enabledPaymentTypeIds ?? []
+   return Object.values(PAYMENT_TYPE).filter(paymentType => enabledIds.includes(paymentType.ID))
+})
+
+watch(() => company.value?.options?.paymentTypeSetting, setting => {
+   if (!company.value) return
+   const enabledIds = setting?.enabledPaymentTypeIds ?? []
+   const defaultId = setting?.paymentTypeIdByDefault
+   const reassignedId = enabledIds.includes(Number(defaultId))
+      ? Number(defaultId)
+      : PAYMENT_TYPE.CASH.ID
+   if (!paymentTypeId.value || !enabledIds.some(id => Number(paymentTypeId.value) === id)) {
+      paymentTypeId.value = reassignedId
+   }
+}, { immediate: true })
