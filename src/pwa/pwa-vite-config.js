4370173e55aba682c257d33b8fe70363d22a054f
Evgeniy-Shabakov <shabakovevgeniy@gmail.com>
2026-09-22 10:02:32 +0300

    pwa

--- src/pwa/pwa-vite-config.js
+++ src/pwa/pwa-vite-config.js
@@ -3,17 +3,6 @@ import { VitePWA } from 'vite-plugin-pwa'
 export function pwaViteConfig(env) {
    return VitePWA({
       registerType: 'autoUpdate',
-      manifest: {
-         name: env.VITE_PWA_NAME,
-         short_name: env.VITE_PWA_NAME,
-         start_url: '/',
-         theme_color: '#ffffff',
-         background_color: '#F5F5F5',
-         display: 'standalone',
-         icons: [
-            { src: '/pwa-icon-192x192.png', sizes: '192x192', type: 'image/png' },
-            { src: '/pwa-icon-512x512.png', sizes: '512x512', type: 'image/png' },
-         ],
-      },
+      manifest: false 
    })
 }
